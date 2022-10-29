e9497eba47b6f7e9f31d7887ee4f1f1e3ada3edd
Nathan Westergard <m800mazdarx8@gmail.com>
2022-10-29 08:24:44 -0600

    Whole File

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import Menu from "./components/Menu";
+import "./App.css";
 
 function App() {
+  const canvasRef = useRef(null);
+  const mouseLocation = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [lineWidth, setLineWidth] = useState(20);
+  const [lineColor, setLineColor] = useState("black");
+  const [lineOpacity, setLineOpacity] = useState(0.1);
+
+  // Initialization when the component
+  // mounts for the first time
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const mouse = canvas.getContext("2d");
+    mouse.lineCap = "round";
+    mouse.lineJoin = "round";
+    mouse.globalAlpha = lineOpacity;
+    mouse.strokeStyle = lineColor;
+    mouse.lineWidth = lineWidth;
+    mouseLocation.current = mouse;
+  }, [lineColor, lineOpacity, lineWidth]);
+
+  // Function for starting the drawing
+  const startDrawing = (event) => {
+    mouseLocation.current.beginPath();
+    mouseLocation.current.moveTo(event.nativeEvent.offsetX, event.nativeEvent.offsetY);
+    setIsDrawing(true);
+  };
+
+  // Function for ending the drawing
+  const endDrawing = () => {
+    mouseLocation.current.closePath();
+    setIsDrawing(false);
+  };
+
+  // Function to keep drawing 
+  const draw = (event) => {
+    if (!isDrawing) {
+      return;
+    }
+    mouseLocation.current.lineTo(event.nativeEvent.offsetX, event.nativeEvent.offsetY);
+    mouseLocation.current.stroke();
+  };
+
+  // Function to clear the canvas
+  const wipeBoard = () => {
+    window.location.reload()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="Title">Paint App</h1>
+        <Menu
+          setLineColor={setLineColor}
+          setLineWidth={setLineWidth}
+          setLineOpacity={setLineOpacity}
+          wipeBoard={wipeBoard}
+        />
+      <div className="draw-area">
+        <canvas
+          onMouseDown={startDrawing}
+          onMouseUp={endDrawing}
+          onMouseMove={draw}
+          ref={canvasRef}
+          width={`2200px`}
+          height={`1000px`}
+        />
+      </div>
     </div>
   );
 }
